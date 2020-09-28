56b961c1f854e997e97b7d12aaa4ac0b27ca6ad3
Kuba <jakub.cichorski@hotmail.com>
2020-09-28 18:47:25 +0200

    cleaner error handling, created .env.example file

--- dbConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports =  {
-    user: 'xelcode',
-    password: 'xelcode',
-    server: '10.100.200.62',
-    database: 'XELCODE',
-    parseJSON: true
-}
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const sql = require('mssql');
-const dbConfig = require('./dbConfig');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    parseJSON: true,
+}
+
+const handleError = (err, res) => {
+    switch (err.name){
+        case 'ConnectionError':
+            res.status(408).json('Błąd połączenia z bazą danych. Upewnij się, że masz połączenie z siecią VPN.');
+            return
+        case 'RequestError':
+            res.status(408).json('Wysyłanie żądania zajęło zbyt długo.');
+            return
+        default:
+            res.status(500).json(err.message);
+            return
+    }
+
+}
 
 // config
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT;
 
 // middleware
 app.use(express.json());
@@ -16,7 +41,9 @@ app.post('/api/v1/getData', (req, res) => {
     const {dateFrom, dateTo, user, status} = req.body;
     try {
         sql.connect(dbConfig, (err) => {
-            if (err) {throw err}
+            if (err) {
+                handleError(err, res);
+            }
             else {
                 try {
                     let request = new sql.Request();
@@ -33,22 +60,28 @@ app.post('/api/v1/getData', (req, res) => {
                     if(status) {
                         sqlQuery.push(`[Status] = '${status}'`);
                     }
-                    let query = `select * from LogHeader where ${sqlQuery.join(' AND ')} for json auto;`;
+                    let query = '';
+                    (!dateFrom && !dateTo && !user && !status)? query = 'select * from LogHeader for json auto;'
+                        :
+                            query = `select * from LogHeader where ${sqlQuery.join(' AND ')} for json auto;`;
+
                     request.query(query, (err, recordset) => {
-                        if (err) {throw err}
+                        if (err) {
+                            handleError(err, res);
+                        }
                         else {
                             try{
                                 let data = recordset.recordsets[0][0];
                                 res.type('application/json');
                                 res.status(200).json(data);
-                            } catch(err){res.status(500).send(err);}
+                            } catch(err){handleError(err, res)}
                         }
                     })
-                } catch(err){res.status(500).send(err)}
+                } catch(err){handleError(err, res)}
                 
             }
         })
-    } catch(err) {res.status(500).send(err)}
+    } catch(err) {handleError(err, res)}
     
 });
 
